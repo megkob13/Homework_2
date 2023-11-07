e3e13d09fa9747dc5ca8fbc825fae50394e42d93
Megan <mko5258@psu.edu>
2023-11-07 17:03:38 -0500

    Added comments

--- Q1/main.cpp
+++ Q1/main.cpp
@@ -14,12 +14,13 @@ using namespace std;
 
 int main()
 {
-	//Use multimap to contain the flight locations (takeoff and departure locations)
+	//Use multimap to contain the flight locations (departure and arrival locations)
 	//as well as distance and unit of distance for the flight
 
+	//multimap< pair< departure location, arrival location >, pair < distance magnitude, distance unit> >
 	multimap< pair<string, string>, pair<int, string> > flights;
-	flights.insert (make_pair (make_pair("SCE", "PHL"), make_pair(160, "mi")));
-	flights.insert (make_pair (make_pair("SCE", "ORD"), make_pair(640, "mi")));
-	flights.insert (make_pair (make_pair("SCE", "EWR"), make_pair(220, "mi")));
+	flights.insert (make_pair (make_pair("SCE", "PHL"), make_pair(160, "miles")));
+	flights.insert (make_pair (make_pair("SCE", "ORD"), make_pair(640, "miles")));
+	flights.insert (make_pair (make_pair("SCE", "EWR"), make_pair(220, "miles")));
 
 }
