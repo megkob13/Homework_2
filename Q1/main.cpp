7ad2ed0609094d92a886e26be45c592d8bb259bb
Megan <mko5258@psu.edu>
2023-11-07 20:30:55 -0500

    Changed container type

--- Q1/main.cpp
+++ Q1/main.cpp
@@ -14,13 +14,16 @@ using namespace std;
 
 int main()
 {
-	//Use multimap to contain the flight locations (departure and arrival locations)
+	//Use map to contain the flight locations (departure and arrival locations)
 	//as well as distance and unit of distance for the flight
 
-	//multimap< pair< departure location, arrival location >, pair < distance magnitude, distance unit> >
-	multimap< pair<string, string>, pair<int, string> > flights;
-	flights.insert (make_pair (make_pair("SCE", "PHL"), make_pair(160, "miles")));
-	flights.insert (make_pair (make_pair("SCE", "ORD"), make_pair(640, "miles")));
-	flights.insert (make_pair (make_pair("SCE", "EWR"), make_pair(220, "miles")));
+	//Using map because the keys should only have one valid value for flight 
+	//distance i.e. there should be no duplicate keys
+
+	//map< pair< departure location, arrival location >, pair < distance magnitude, distance unit> >
+	map< pair<string, string>, pair<int, string> > flights;
+	flights.insert (make_pair ( make_pair("SCE", "PHL"), make_pair(160, "miles" )));
+	flights.insert (make_pair ( make_pair("SCE", "ORD"), make_pair(640, "miles" )));
+	flights.insert (make_pair ( make_pair("SCE", "EWR"), make_pair(220, "miles" )));
 
 }
