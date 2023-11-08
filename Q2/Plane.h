46c32ea046a722e486cb426fe01dd00d219d4418
Megan <mko5258@psu.edu>
2023-11-07 21:38:23 -0500

    Created class in .h file, copied over code from Q1 as needed

---
+++ Q2/Plane.h
@@ -0,0 +1,68 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+#include <random>
+
+class Plane
+{
+public:
+
+//Constructor that takes in two strings "from" and "to" as input arguments
+	Plane(string from, string to);
+
+//Virtual deconstructor
+	virtual ~Plane();
+
+//Function named operate with double variables "dt" as input, return nothing
+	void operate(double dt);
+
+//Get functions
+	double getpos();
+	double getvel();
+	double getloiter_time();
+	string getorigin();
+	string getdestination();
+	bool getat_SCE();
+
+//Set functions
+	void setvel(double);
+	void setloiter_time(double);
+
+//Function named "distance_to_SCE" that returns double
+	double distance_to_SCE();
+
+//Virtual functions
+	virtual double time_on_ground();
+	virtual string plane_type();
+
+//Static function named "draw_from_normal_dist" that takes in two
+//double variables (mean and standard deviation), and returns back
+//a random number from a normal distribution
+	static double draw_from_normal_dist(double mean, double standard_deviation);
+
+private:
+
+//Double variables named "pos", "vel", "distance", and "loiter_time"
+	double pos;
+	double vel;
+	double distance;
+	double loiter_time;
+
+//Boolean variable named "at_SCE"
+	bool at_SCE;
+
+//String variables named "origin" and "destination"
+	string origin;
+	string destination;
+
+//Container from Question 1
+	map< pair<string, string>, pair<int, string> > flights;
+
+protected:
+
+//Double variable named "wait_time"
+	double wait_time;
+};
--- Q2/main.cpp
+++ Q2/main.cpp
@@ -1,2 +1,27 @@
 //Homework 2
 //Question 2
+//Create class named "Plane" with given class members
+
+//CLASS CREATED IN "Plane.h"
+//COPYING AND PASTING FROM FILES FROM PREVIOUS QUESTIONS TO BUILD UP FULL CODE
+
+
+#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+#include <random>
+#include "Plane.h"
+
+
+using namespace std;
+
+int main()
+{
+	//Container from Q1
+	map< pair<string, string>, pair<int, string> > flights;
+	flights.insert (make_pair ( make_pair("SCE", "PHL"), make_pair(160, "miles" )));
+	flights.insert (make_pair ( make_pair("SCE", "ORD"), make_pair(640, "miles" )));
+	flights.insert (make_pair ( make_pair("SCE", "EWR"), make_pair(220, "miles" )));
+
+}
